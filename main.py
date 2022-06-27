21395d38264bd288557ed05442f191271154a8de
Михаил Валерьевич <mr.mihailmorozv@yandex.ru>
2022-06-27 08:26:09 +0300

    Осталось придумать как нормализовать входы

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-from math import exp, log
+from math import exp
 
 
-def sigmoid(x):
-    #return 1 / (1 + exp(-x))
-    #return (2 / (1 + exp(-2 * x))) - 1
-    return max(0.0, x)*0.1
+weights = [[[-0.09233616, -0.09484169, -0.5137412 , -0.38164628,  0.52477765],
+       [ 0.18182069,  0.62269163,  0.04443356, -0.08441257, -0.23126258],
+       [ 0.26208717, -1.391956  , -0.18106905, -0.16377348, -0.91910046]],
+           [[0.03227055, -1.0830162, -0.45987815, 0.21449643, -0.15913789],
+            [0.2648304, 1.1677712, 0.79938, -0.35905856, 0.44171396],
+            [-0.5499226, -0.3998966, -0.597909, -0.70432305, 0.16170573],
+            [-0.02145684, 0.04332811, -0.20603317, -0.12480557, -0.17770183],
+            [0.08298005, 0.4372738, -0.23406775, 0.00777137, 0.14510638]]]
+
+bias = [[-0.60071355,  0.02919444, -0.00400464,  0.        ,  0.13409168],
+        [-0.33380628,  0.2936416 ,  0.7279624 , -1.0291904 ,  0.34147626]]
+
+
+sex = input("Введите ваш пол м/ж: ")
+if sex == "м":
+    sex = 0
+else:
+    sex = 1
+age = int(input("Введите ваш возраст: "))
+time = int(input("Введите планируемое время для соцсетей: "))
+
+
+def sort_inputs(x):
+    dct = {"vk": x[0],
+           "ok": x[1],
+           "in": x[2],
+           "yt": x[3],
+           "tt": x[4]}
+    sorted_tuple = sorted(dct.items(), key = lambda x: x[1])
+    lst = list(sorted_tuple)[::-1]
+    for i in lst:
+        if i[0] == "vk":
+            print(f"Вам подходит ВК на {round(100 * i[1], 1)}%")
+        elif i[0] == "ok":
+            print(f"Вам подходят Одноклассники на {round(100 * i[1], 1)}%")
+        elif i[0] == "in":
+            print(f"Вам подходит Интаграмм* на {round(100 * i[1], 1)}%")
+        elif i[0] == "yt":
+            print(f"Вам подходит YouTube на {round(100 * i[1], 1)}%")
+        elif i[0] == "tt":
+            print(f"Вам подходит TikTok на {round(100 * i[1], 1)}%")
+
+
+def relu(x):
+    return max(0.0, x)
 
 
 def softmax(x):
@@ -15,52 +56,30 @@ def softmax(x):
     return total
 
 
-def logloss(output, result):
-    total = 0
-    for o, y in zip(output, result):
-        total -= y * log(o)
-    return total
-
 class NeuralNetwork:
     def __init__(self, weight, bias):
-        self.weight = weight
-        self.bias = bias
+        self.weight_h = weight[0]
+        self.bias_h = bias[0]
+        self.weight_o = weight[1]
+        self.bias_o = bias[1]
 
     def result(self, inputs):
-        h1 = sigmoid(inputs[0] * self.weight[0] + inputs[1] * self.weight[1] + inputs[2] * self.weight[2] + self.bias[0])
-        h2 = sigmoid(inputs[0] * self.weight[3] + inputs[1] * self.weight[4] + inputs[2] * self.weight[5] + self.bias[1])
-        h3 = sigmoid(inputs[0] * self.weight[6] + inputs[1] * self.weight[7] + inputs[2] * self.weight[8] + self.bias[2])
-        h4 = sigmoid(inputs[0] * self.weight[24] + inputs[1] * self.weight[25] + inputs[2] * self.weight[26] + self.bias[8])
+        h1 = relu(inputs[0] * self.weight_h[0][0] + inputs[1] * self.weight_h[1][0] + inputs[2] * self.weight_h[2][0] + self.bias_h[0])
+        h2 = relu(inputs[0] * self.weight_h[0][1] + inputs[1] * self.weight_h[1][1] + inputs[2] * self.weight_h[2][1] + self.bias_h[1])
+        h3 = relu(inputs[0] * self.weight_h[0][2] + inputs[1] * self.weight_h[1][2] + inputs[2] * self.weight_h[2][2] + self.bias_h[2])
+        h4 = relu(inputs[0] * self.weight_h[0][3] + inputs[1] * self.weight_h[1][3] + inputs[2] * self.weight_h[2][3] + self.bias_h[3])
+        h5 = relu(inputs[0] * self.weight_h[0][4] + inputs[1] * self.weight_h[1][4] + inputs[2] * self.weight_h[2][4] + self.bias_h[4])
 
-        v1 = sigmoid(h1 * self.weight[9] + h2 * self.weight[10] + h3 * self.weight[11] + h4 * self.weight[27] + self.bias[3])
-        v2 = sigmoid(h1 * self.weight[12] + h2 * self.weight[13] + h3 * self.weight[14] + h4 * self.weight[28] + self.bias[4])
-        v3 = sigmoid(h1 * self.weight[15] + h2 * self.weight[16] + h3 * self.weight[17] + h4 * self.weight[29] + self.bias[5])
-        v4 = sigmoid(h1 * self.weight[18] + h2 * self.weight[19] + h3 * self.weight[20] + h4 * self.weight[30] + self.bias[6])
-        v5 = sigmoid(h1 * self.weight[21] + h2 * self.weight[22] + h3 * self.weight[23] + h4 * self.weight[31] + self.bias[7])
+        v1 = h1 * self.weight_o[0][0] + h2 * self.weight_o[1][0] + h3 * self.weight_o[2][0] + h4 * self.weight_o[3][0] + h5 * self.weight_o[4][0] + self.bias_o[0]
+        v2 = h1 * self.weight_o[0][1] + h2 * self.weight_o[1][1] + h3 * self.weight_o[2][1] + h4 * self.weight_o[3][1] + h5 * self.weight_o[4][1] + self.bias_o[1]
+        v3 = h1 * self.weight_o[0][2] + h2 * self.weight_o[1][2] + h3 * self.weight_o[2][2] + h4 * self.weight_o[3][2] + h5 * self.weight_o[4][2] + self.bias_o[2]
+        v4 = h1 * self.weight_o[0][3] + h2 * self.weight_o[1][3] + h3 * self.weight_o[2][3] + h4 * self.weight_o[3][3] + h5 * self.weight_o[4][3] + self.bias_o[3]
+        v5 = h1 * self.weight_o[0][4] + h2 * self.weight_o[1][4] + h3 * self.weight_o[2][4] + h4 * self.weight_o[3][4] + h5 * self.weight_o[4][4] + self.bias_o[4]
 
-        v = [v1, v2, v3, v4, v5]
+        return softmax([v1, v2, v3, v4, v5])
 
-        return softmax(v)
-
-
-weights = [7.301664331320012, 0.570223061496471, 7.434506160761314, 5.488233630360416, 0.8539867662610575, 5.090958102107124, 12.819564122674779, 1.0547337203491507, 11.389471951561879, 3.010732702195124, 2.112025503666808, 3.8042183319935745, 2.3850964355471973, 2.310809535084475, 4.074887535629562, 6.9800117966623985, 4.938610240311849, 12.203170891674146, 2.514507600288209, 1.7622616922129342, 4.1466944703669695, 2.244863095664602, 1.6486849515258282, 4.161761216115949, 1.6586887692400494, 0.6602458775416515, 0.9786068433510086, 0.3463435658134911, 0.8115135223747391, 0.10717075513125174, 0.32634868493737246, 0.18209515236159823]
-bias = [25.028931871373775, 18.228043658970336, 42.845561539370436, 1.8424682220548045, 1.172744319041811, 9.63450647702029, 1.5122661568328968, 1.3454908662177905, 3.509851910503541]
-
-sex = input("Введите ваш пол м/ж: ")
-if sex == "м":
-    sex = 0
-else:
-    sex = 1
-age = int(input("Введите ваш возраст: "))
-time = int(input("Введите планируемое время для соцсетей: "))
 
 nn = NeuralNetwork(weights, bias)
-res = nn.result([age/100, sex/100, time/100])
+res = nn.result([age, sex*100, time])
 
-print(f"Вам подходит ВК на {res[0]}%")
-print(f"Вам подходят Одноклассники на {res[1]}%")
-print(f"Вам подходит Интаграмм* на {res[2]}%")
-print(f"Вам подходит YouTube на {res[3]}%")
-print(f"Вам подходит TikTok на {res[4]}%")
-
-
+sort_inputs(res)
--- train.py
+++ train.py
@@ -1,81 +1,32 @@
-import math
-import random
-
+import numpy as np
 import randomuserlist
-
-# x1, x2, x3 - входы age, sex(0 = male, 1 = female) и time соответственно
-# h1, h2 - слои
-# wx, bx - веса и смещения сответсвенно
-#
-# h1*w31 + h2*w32 + b4
-# where: hx = x1/2 * wx1 + w1/2 * wx2 + bx -->
-# --> (x1*w11 + x2*w12 + x3*w13 + b1)*w31 + (x3*w21 + x2*w22 + x1*w23 + b2)*w32 + b3
+import random
+from tensorflow import keras
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def get_user_list(lst_len):
+    lst = randomuserlist
+    inputs_and_result = [lst.generateList(lst_len, "vk") + lst.generateList(lst_len, "ok") + lst.generateList(lst_len, "inst") + lst.generateList(lst_len, "yt") + lst.generateList(lst_len, "tt")][0]
+    random.shuffle(inputs_and_result)
+
+    return [[i[0], i[1] * 100, i[2]] for i in inputs_and_result], [i[3] for i in inputs_and_result]
 
 
-class Layer:
-    def __init__(self, weight, bias):
-        self.weight = weight
-        self.bias = bias
+model = keras.Sequential()
+hidden_layer = keras.layers.Dense(5, activation = 'relu', input_shape = (3,), name = 'hidden')
+model.add(hidden_layer)
+output_layer = keras.layers.Dense(5, activation = 'softmax', name = 'end')
+model.add(output_layer)
+sgd = keras.optimizers.SGD(learning_rate = 0.001)
+model.compile(optimizer = sgd, loss = 'categorical_crossentropy')
+lst = get_user_list(40)
 
-    def feedforward(self, inputs):
-        layer_1_1 = sigmoid(float(inputs[0]) * self.weight[0][0] + float(inputs[1]) * self.weight[0][1] + float(inputs[2]) * self.weight[0][2] + self.bias[0])
-        layer_1_2 = sigmoid(float(inputs[2]) * self.weight[1][0] + float(inputs[1]) * self.weight[1][1] + float(inputs[0]) * self.weight[1][2] + self.bias[1])
-        return sigmoid(layer_1_1 * self.weight[2][0] + layer_1_2 * self.weight[2][1] + self.bias[2])
-
-    def loss(self, result, true_result):
-        sm = 0
-        n = len(result)
-        for a, b in zip(result, true_result):
-            sm += (b - a) ** 2
-
-        return sm / n
-
-    def train(self, inputs, result):
-        learn_rate = 1
-        epochs = 2000
-
-        for epoch in range(epochs):
-            for x, y in zip(inputs, result):
-                self.weight[0][0] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][0]*x[0]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.weight[0][1] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][0]*x[1]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.weight[0][2] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][0]*x[2]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.bias[0] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][0]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-
-                self.weight[1][0] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][1]*x[2]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.weight[1][1] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][1]*x[1]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.weight[1][2] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][1]*x[0]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.bias[1] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*self.weight[2][1]*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-
-                self.weight[2][0] -= learn_rate * (2*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))*(-0.999999999831127*self.bias[0] - 0.999999999831127*self.weight[0][0]*x[0] - 0.999999999831127*self.weight[0][1]*x[1] - 0.999999999831127*self.weight[0][2]*x[2])/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.weight[2][1] -= learn_rate * (2*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))*(-0.999999999831127*self.bias[1] - 0.999999999831127*self.weight[1][0]*x[2] - 0.999999999831127*self.weight[1][1]*x[1] - 0.999999999831127*self.weight[1][2]*x[0])/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-                self.bias[2] -= learn_rate * (-1.99999999966225*2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0]))*(y - 1/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1))/(2.718281828**(-self.bias[2] - self.weight[2][0]*(self.bias[0] + self.weight[0][0]*x[0] + self.weight[0][1]*x[1] + self.weight[0][2]*x[2]) - self.weight[2][1]*(self.bias[1] + self.weight[1][0]*x[2] + self.weight[1][1]*x[1] + self.weight[1][2]*x[0])) + 1)**2)
-
-    def getValues(self):
-        print("Веса ", self.weight)
-        print("Смещения ", self.bias)
+model.fit(np.array(lst[0]), np.array(lst[1]), epochs = 200, steps_per_epoch = 200)
+results = model.predict([[30, 100, 35], [40, 100, 14], [30, 100, 26], [30, 0, 48], [30, 100, 32]], steps = 1, verbose = 0)
 
 
-lst = randomuserlist
-lst_len = 200
+for i in results:
+    print([round(a * 100, 1) for a in i])
 
-inputs_and_result = [lst.generateList(lst_len, "vk") + lst.generateList(lst_len, "ok") + lst.generateList(lst_len, "inst") + lst.generateList(lst_len, "yt") + lst.generateList(lst_len, "tt")][0]
-inputs = [i[:3] for i in inputs_and_result]
-result = [i[3] for i in inputs_and_result]
-
-bias = []
-weights = [[], [], []]
-
-for i in range(len(weights)-1):
-    weights[i] = [random.random(), random.random(), random.random()]
-    bias.append(random.random())
-weights[-1] = [random.random(), random.random()]
-bias.append(random.random())
-
-
-l = Layer(weights, bias)
-l.train(inputs, result)
-l.getValues()
+for layer in model.layers:
+    print(layer.get_config(), layer.get_weights())
